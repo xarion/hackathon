9ce5c42f6c0c98cb1d65a74d04fc88a82fe4eb99
xarion <erdicalli@gmail.com>
2016-11-27 08:59:59 +0100

    things

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,23 @@ var giant_hurt_audio = new Audio('audio/NPC/giant/giant3.wav');
 var player = "#p1";
 var skeleton = "#p2";
 
+var stimuli_root = "graphics/items/";
+var stimuli_list = ["gemBlue.png", "gemGreen.png", "gemRed.png", "gemYellow.png", "bomb.png", "cactus.png", "chain.png", "coinBronze.png", "fireball.png", "gemBlue.png", "keyGreen.png", "mushroomRed.png", "plant.png", "rock.png", "star.png", "weight.png"];
+
+var number_of_stimuli = 4;
+var stimuli_present_for_ms = 2000;
+var fade_duration = 500;
+var n_back = 2;
+
+var back_list = [];
+var global_step = 0;
+var current_stimuli = -1;
+var game_running = false;
+var turn_played = false;
+
 
 function heal(player) {
+    console.log("heal");
     $(player).removeClass("stand").addClass("heal");
     setTimeout(function () {
         heal_damage(player, 20);
@@ -14,6 +29,7 @@ function heal(player) {
 }
 
 function attack(attacker) {
+    console.log("attack");
     if (Math.random() > 0.5) {
         slash(attacker);
     } else {
@@ -30,7 +46,6 @@ function slash(player) {
 
 }
 
-
 function bow(player) {
     $(player).removeClass("stand").addClass("bow");
     setTimeout(function () {
@@ -46,15 +61,15 @@ function hurt(player) {
     }
 }
 
+function kill(player) {
+    $(player).removeClass("stand").addClass("dead");
+}
 
 function heal_damage(player, hit_points) {
-    console.log(hit_points);
     var target_player = $(player);
     var target_player_health_bar = target_player.siblings(".health");
     var target_health = parseInt(target_player_health_bar.attr("aria-valuenow"));
-    console.log(target_health);
     target_health += parseInt(hit_points);
-    console.log(target_health);
     if (target_health > 100) {
         target_health = 100;
     }
@@ -69,26 +84,15 @@ function deal_damage(player, damage) {
     target_health -= damage;
     if (target_health <= 0) {
         target_health = 0;
-        stop_game(player);
+        kill(target_player);
+        stop_game();
+    } else {
+        hurt(target_player);
     }
     target_player_health_bar.attr("aria-valuenow", target_health);
     target_player_health_bar.css("width", target_health + "%");
-    hurt(target_player);
 }
 
-var stimuli_root = "graphics/items/";
-var stimuli_list = ["gemBlue.png", "gemGreen.png", "gemRed.png", "gemYellow.png", "bomb.png", "cactus.png", "chain.png", "coinBronze.png", "fireball.png", "gemBlue.png", "keyGreen.png", "mushroomRed.png", "plant.png", "rock.png", "star.png", "weight.png"];
-
-var number_of_stimuli = 4;
-var stimuli_present_for_ms = 2000;
-var fade_duration = 500;
-var n_back = 2;
-
-var back_list = [];
-var global_step = 0;
-var current_stimuli = -1;
-var game_running = false;
-var turn_played = false;
 
 function put_item_to_backlist(stimuli_id) {
     back_list[global_step % n_back] = stimuli_id;
@@ -99,11 +103,9 @@ function current_stimuli_is_nback() {
 }
 
 function present_stimuli() {
+    console.log("present_stimuli");
     var stimuli_id = Math.round(Math.random() * (number_of_stimuli - 1));
-    console.log(stimuli_id);
-    console.log(stimuli_list[stimuli_id]);
-    $("#stimuli-img").attr("src", stimuli_root + stimuli_list[stimuli_id]);
-    $("#stimuli-img").fadeIn(fade_duration);
+    $("#stimuli-img").attr("src", stimuli_root + stimuli_list[stimuli_id]).fadeIn(fade_duration);
     turn_played = false;
     return stimuli_id;
 }
@@ -129,6 +131,19 @@ function stimuli_loop() {
     }, stimuli_present_for_ms - fade_duration);
 }
 
+function reset() {
+    back_list = [];
+    global_step = 0;
+    current_stimuli = -1;
+    game_running = false;
+    turn_played = false;
+    $("#p1").removeClass("bow").removeClass("hurt").removeClass("slash").removeClass("heal").removeClass("dead").addClass("stand");
+    $("#p2").removeClass("bow").removeClass("hurt").removeClass("slash").removeClass("heal").removeClass("dead").addClass("stand");
+    $(".health").attr("aria-valuenow", 100).css("width", "100%").hide();
+    $('.action-bar').hide();
+    $('.portrait').hide();
+}
+
 function attack_action() {
     var attacker;
     if (current_stimuli_is_nback()) {
@@ -139,7 +154,6 @@ function attack_action() {
     attack(attacker);
 }
 
-
 function heal_action() {
     if (current_stimuli_is_nback()) {
         heal(player);
@@ -149,47 +163,60 @@ function heal_action() {
 }
 
 function start_game() {
-    game_running = true;
-    stimuli_loop();
-    $(".stimuli").fadeIn(fade_duration);
-    $("#stimuli-img").fadeIn(fade_duration);
-}
-
-function stop_game(winner) {
-    game_running = false;
-    if (winner == player) {
-        alert("Congratulations, you won!");
-    } else {
-        alert("Practice makes perfect, keep playing.")
-    }
-    heal_damage(skeleton, 100);
-    heal_damage(player, 100);
-}
-
-function preload() {
-    $(stimuli_list).each(function () {
-        $('<img />').attr('src', stimuli_root + this).appendTo('body').css('display', 'none');
+    console.log("start game");
+    $(".portrait").fadeIn(fade_duration, function () {
+        $(".health").fadeIn(fade_duration);
     });
+    $(".action-bar").fadeIn(fade_duration*3, function () {
+            game_running = true;
+            stimuli_loop();
+            $(".stimuli").fadeIn(fade_duration);
+            // $("#stimuli-img").fadeIn(fade_duration);
+        });
 }
-// Usage:
 
+function stop_game() {
+    game_running = false;
+}
 
 $(document).ready(function () {
 
+    $(".play-button-container").on("click", function () {
+        $("#home-page").fadeOut(1000, function () {
+            $("#game-page").fadeIn(1000, function () {
+                $(".animation").addClass("walk").show();
+            });
+        });
+    });
+
+    $(".animation").on("webkitAnimationEnd oanimationend msAnimationEnd animationend",
+        function () {
+            var thiz = $(this);
+            thiz.fadeOut(1500, function () {
+                console.log("running start_game");
+                start_game();
+                thiz.hide();
+            });
+
+        }
+    );
     $(".portrait").on("webkitAnimationEnd oanimationend msAnimationEnd animationend",
         function () {
             var thiz = $(this);
-            thiz.removeClass("bow").removeClass("hurt").removeClass("slash").removeClass("heal").addClass("stand");
-            // rest is for fun purposes
-            // setTimeout(function () {
-            //     slash(thiz);
-            // }, 1000);
+            if (!thiz.hasClass("dead")) {
+                thiz.removeClass("bow").removeClass("hurt").removeClass("slash").removeClass("heal").addClass("stand");
+            } else if (thiz.hasClass("dead")) {
+                $("#game-page").fadeOut(1000, function () {
+                    $("#home-page").fadeIn(1000, function () {
+                        reset();
+                    });
+                });
+            }
         }
     );
     preload();
-    // present_stimuli();
-    // background_music.loop = true;
-    // background_music.play()
+    background_music.loop = true;
+    background_music.play();
 
     $(document).keypress(function (event) {
         if (event.which == 49 || event.which == 50) {
@@ -205,4 +232,11 @@ $(document).ready(function () {
         }
     });
 });
+
+
+function preload() {
+    $(stimuli_list).each(function () {
+        $('<img />').attr('src', stimuli_root + this).appendTo('body').css('display', 'none');
+    });
+}
 
